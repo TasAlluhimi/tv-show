a6aa434be08eb8bde1749fa854b6672b4f235dd1
tasalluhimi <tasneem.luhimi@gmail.com>
2023-11-06 12:01:00 +0300

    tv show

---
+++ main.js
@@ -0,0 +1,47 @@
+let screen_img = document.getElementById("screen-img")
+
+
+let one = document.getElementById("one")
+one.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg'
+})
+
+let two = document.getElementById("two")
+two.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg'
+})
+
+let three = document.getElementById("three")
+three.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg'
+})
+
+let four = document.getElementById("four")
+four.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg'
+})
+
+let five = document.getElementById("five")
+five.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg'
+})
+
+let six = document.getElementById("six")
+six.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg'
+})
+
+let seven = document.getElementById("seven")
+seven.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg'
+})
+
+let eight = document.getElementById("eight")
+eight.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg'
+})
+
+let nine = document.getElementById("nine")
+nine.addEventListener("click", ()=>{
+    screen_img.src = 'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg'
+})
